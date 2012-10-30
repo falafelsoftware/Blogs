3faf142ab925424b1fc575bd525c62c408463ec4
Falafel Software <info@falafel.com>
2012-10-30 09:50:16 -0400

    Added Win8DeclarativeComboBox
    Blog Demo for XAML / Windows 8 Declarative ComboBoxes

---
+++ Win8DeclarativeComboBox/Win8DeclarativeComboBox/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace Win8DeclarativeComboBox.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int selectedDirection;
+        public int SelectedDirection
+        {
+            get
+            {
+                return this.selectedDirection;
+            }
+            set
+            {
+                this.selectedDirection = value;
+                this.OnPropertyChanged("SelectedDirection");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
